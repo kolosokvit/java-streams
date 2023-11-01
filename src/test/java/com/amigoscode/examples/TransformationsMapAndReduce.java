25c31cc6bb6a41e4c9b3055e18de6b85c5e4c097
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-01 15:12:54 +0300

    map() and reduce()

--- src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -17,16 +18,32 @@ public class TransformationsMapAndReduce {
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<PersonDTO> personDTOS = people.stream()
+                .map(person -> {
+                    return new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+                })
+                .toList();
+        personDTOS.forEach(System.out :: println);
     }
 
     @Test
     void mapToDoubleAndFindAverageCarPrice() throws IOException {
         List<Car> cars = MockData.getCars();
+        OptionalDouble averagePrice = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average();
+        System.out.println(averagePrice);
     }
 
     @Test
     public void reduce() {
+        //Performs a reduction on the elements of this stream, using the provided identity value
+        // and an associative accumulation function, and returns the reduced value.
         int[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+
+
+        int sum = Arrays.stream(integers).reduce(0, (subtotal, next) -> subtotal + next);
+        System.out.println(sum);
     }
 }
 
