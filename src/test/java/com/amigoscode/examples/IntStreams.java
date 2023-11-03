604903ed3b4bf17df9c433865353b82a2921aa0a
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-03 10:33:52 +0300

    IntStreams

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -13,15 +13,35 @@ public class IntStreams {
 
     @Test
     public void range() throws Exception {
+        System.out.println("with for i");
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("with int stream exclusive");
+        IntStream.range(0, 10).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+
+        System.out.println("with int stream inclusive");
+        IntStream.rangeClosed(0, 10).forEach(i -> System.out.print(i + " "));
+        System.out.println();
     }
 
     // Loop through people using IntStream
     @Test
     public void rangeIteratingLists() throws Exception {
         List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    System.out.println(people.get(index));
+                });
     }
 
     @Test
     public void intStreamIterate()  {
+        IntStream.iterate(0, value -> value + 1)
+                .limit(11)
+                .forEach(System.out::println);
     }
 }
