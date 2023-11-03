c8ece1a9594184d8fa0a2064b43e98242d995823
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-03 10:14:24 +0300

    Grouping

--- src/test/java/com/amigoscode/examples/GroupingData.java
+++ src/test/java/com/amigoscode/examples/GroupingData.java
@@ -5,16 +5,26 @@ import com.amigoscode.beans.Car;
 import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class GroupingData {
 
     @Test
     public void simpleGrouping() throws Exception {
-        List<Car> cars = MockData.getCars();
+        //grouping cars by year
+        Map<Integer, List<Car>> carsByYear = MockData.getCars()
+                .stream()
+                .collect(Collectors.groupingBy(Car::getYear));
+        carsByYear.forEach((year, cars) -> {
+            System.out.println("Year " + year);
+            cars.forEach(System.out::println);
+            System.out.println("---------------------");
+        });
     }
 
     @Test
@@ -30,6 +40,20 @@ public class GroupingData {
                 "Alex",
                 "Alex"
         );
-    }
 
+        //grouping and counting names
+        Map<String, Long> map = names.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting())
+                );
+        System.out.println(map);
+
+        //grouping and counting colors
+        Map<String, Long> colors = MockData.getCars()
+                .stream()
+                .collect(Collectors.groupingBy(Car::getColor,
+                        Collectors.counting()));
+        System.out.println(colors);
+    }
 }
